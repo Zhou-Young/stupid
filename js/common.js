30af67e771167a251b16ad267df6c11a34d4921a
heyaaha <181772441@qq.com>
2017-10-22 16:17:30 +0800

    v1.1
    1.1

--- js/common.js
+++ js/common.js
@@ -12,12 +12,31 @@ function g(elem){
 
 }
 
-//很蠢的图片预加载方法
-(function stupidPreLoad(){
-    var mainhtml=[];
-    for(var i=0;i<data.length;i++){
-        var _html='<img src="img/'+data[i].img+'.jpg" width="222px">';
-        mainhtml.push(_html);
-    }
-    g('#stupidPreLoad').innerHTML = mainhtml.join(' ');
-})();
+////很蠢的图片预加载方法
+//(function stupidPreLoad(){
+//    var mainhtml=[];
+//    for(var i=0;i<data.length;i++){
+//        var _html='<img src="img/'+data[i].img+'.jpg" width="222px">';
+//        mainhtml.push(_html);
+//    }
+//    g('#stupidPreLoad').innerHTML = mainhtml.join(' ');
+//})();
+
+
+function show(classN){
+    var _classN = g("#"+classN).className.split(' ');
+    _classN[_classN.length-1] = 'show';
+    g("#"+classN).className = _classN.join(' ');
+}
+
+function hide(classN){
+    var _classN = g("#"+classN).className.split(' ');
+    _classN[_classN.length-1] = 'hidden';
+    g("#"+classN).className = _classN.join(' ');
+}
+
+function magic(hide1,hide2,mshow){
+    hide(hide1);
+    hide(hide2);
+    show(mshow);
+}
